00393b9aa3f333ae87142a8f26df6b3ab6794a15
HHL110120 <jack18@same.com>
2018-04-25 16:48:48 +0800

    添加对类方法的拦截

--- Example/JKUBSAspects/JKViewController.m
+++ Example/JKUBSAspects/JKViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "JKViewController.h"
+#import <JKUBSAspects/JKUBSAspects.h>
 
 @interface JKViewController ()
 
@@ -18,7 +19,38 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+  UIButton *button = [UIButton new];
+[button setTitle:@"点击" forState:UIControlStateNormal];
+[button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+button.frame = CGRectMake(0, 0, 100, 30);
+button.center = self.view.center;
+[button addTarget:self action:@selector(buttonClicked) forControlEvents:UIControlEventTouchUpInside];
+[self.view addSubview:button];
+ 
+    [self aspect_hookSelector:@selector(viewWillAppear:) withOptions:JKUBSAspectPositionBefore usingBlock:^(id<JKUBSAspectInfo> data){
+        NSLog(@"MMM_1");
+    } error:nil];
+    
+    [JKViewController aspect_hookClassSelector:@selector(doSomething) withOptions:JKUBSAspectPositionBefore usingBlock:^(id<JKUBSAspectInfo> data){
+        NSLog(@"doSomething_1");
+    } error:nil];
+
 }
+
+- (void)viewWillAppear:(BOOL)animated{
+    NSLog(@"MMM");
+}
+
+#pragma mark - - - - UIEvent - - - -
+- (void)buttonClicked{
+    [JKViewController doSomething];
+}
+
++ (void)doSomething{
+    NSLog(@"doSomething");
+}
+
+
 
 - (void)didReceiveMemoryWarning
 {
--- JKUBSAspects.podspec
+++ JKUBSAspects.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'JKUBSAspects'
-  s.version          = '0.1.3'
+  s.version          = '0.1.4'
   s.summary          = 'this is a tool to swizzle method'
 
 # This description is used to generate tags and improve search results.
